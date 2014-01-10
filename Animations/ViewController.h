e329b11cb58668103f66c5e1844436c87bb5c2a3
Oleg Trakhman <Sky@Olegs-MacBook-Pro.local>
2014-01-10 14:52:45 +0400

    Initial Commit

---
+++ Animations/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Animations
+//
+//  Created by Oleg Trakhman on 1/10/14.
+//  Copyright (c) 2014 animations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
